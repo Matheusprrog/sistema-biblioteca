2d81efbefda1cae1f1f5bd88da94e7ca6792912c
Matheusprrog <matheusaraujossts@gmail.com>
2026-03-28 09:48:40 -0300

    Sistema de bilioteca com atenticação e controle de perfis

---
+++ src/biblioteca/enums/Cargo.java
@@ -0,0 +1,4 @@
+package biblioteca.enums;
+
+public enum Cargo {
+}
---
+++ src/biblioteca/model/Livro.java
@@ -0,0 +1,27 @@
+package biblioteca;
+
+public class livro {
+
+
+    public class Livro {
+        String autor;
+        String descricao_livro;
+        String data_lancamento;
+        String titulo;
+
+      public Livro(String titulo, String autor, String descricao_livro, String data_lancamento){
+
+          this.titulo = titulo;
+          this.autor = autor;
+          this.descricao_livro = descricao_livro;
+          this.data_lancamento = data_lancamento;
+
+
+
+
+      }
+    }
+
+
+}
+
